b863d3683881c0769477caf489b54d9367ba3b95
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-03 15:26:42 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,19 +4,20 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
-    def print_courses
-      self.make_courses
-      Course.all.each do |course|
-        if course.title
-          puts "Title: #{course.title}"
-          puts "  Schedule: #{course.schedule}"
-          puts "  Description: #{course.description}"
-        end
+  def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
       end
     end
-  
+  end
+  def get_page
+  end
+  def get_courses
+  end
+  def make_courses
+  end
 end
-
-
-
